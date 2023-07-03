ec3ee2b9ee945d0c2df26d681c281f1d56dba089
Juan Carlos <animashorse3@yahoo.com.mx>
2023-07-03 15:22:49 -0600

    Servidor testeado y funcionando

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+"use strict";
+
 /**
  * Import function triggers from their respective submodules:
  *
@@ -5,15 +9,66 @@
  * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+*/
 
+// Librerías
 const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const logger = require("firebase-functions/logger");
+const express = require("express");
+const path = require("path");
+const {WebhookClient} = require("dialogflow-fulfillment");
+const serviceAccount = require("./config/amc-chatbot-hxqi-a17e81df7024.json");
+const admin = require("firebase-admin");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Configuración del servidor
+const server = express();
+server.use(express.urlencoded({extended: true}));
+server.use(express.json());
+server.use("/img", express.static(path.join(__dirname, "/img")));
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+// Inicializar cuenta de servicio de Firebase
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} catch (error) {
+  console.log("Error de inicialización de la cuenta de servicio de Firebase: " + error);
+}
+
+// Si alguien quiere acceder al directorio raiz (método GET)
+server.get("/", (req, res) => {
+  return res.json("Buenas tardes, le saluda el asistente virtual de Arias Medical Clinic. Esta no es la forma correcta de interactuar conmigo. Si necesita información sobre el tratamiento puede hacerlo desde WhatsApp o Messenger.");
 });
+
+// Acceso correcto al agente (método POST)
+server.post("/amcbot", (req, res) => {
+  const agent = new WebhookClient({request: req, response: res});
+
+  // Default Welcome Intent
+  function welcome(agent) {
+    agent.add("Saludos cordiales de parte del Dr. Luis René Arias Villarroel. Soy un asistente virtual listo para resolver sus dudas.");
+    agent.add("Si tiene alguna pregunta sobre el tratamiento, información sobre nuestros servicios o desea agendar una cita, Estoy para servirle.");
+  }
+
+  // Default Fallback Intent
+  function fallback(agent) {
+    agent.add("Una disculpa, no le entendí bien. ¿Podría repetírmelo?");
+  }
+
+  const intentMap = new Map();
+  intentMap.set("Default Welcome Intent", welcome);
+  intentMap.set("Default Fallback Intent", fallback);
+
+  agent.handleRequest(intentMap);
+});
+
+const production = true;
+
+if (production) {
+  exports.amcbot = onRequest(server);
+} else {
+  // Para ejecutar el servidor en local y hacer pruebas
+  server.listen(process.env.PORT || 8000, () => {
+    console.log("Servidor local funcionando!");
+  });
+}
